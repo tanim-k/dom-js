39f6fecacc468c17e64884871cbfb35074125acb
tanim-k <tanimkhan877@gmail.com>
2022-02-07 16:00:56 +0600

    dom dom dom laga

--- js/4th.js
+++ js/4th.js
@@ -1,1 +1,26 @@
-console.log('dfjkskajfhkhjjkdfhjdfhdjfh')
+console.log('im from summery js file');
+
+const blogs = document.getElementsByTagName('p');
+/* console.log(blogs); */
+// same work with for loop //
+for(const blog of blogs) {
+    // console.log(blog);
+    blog.style.backgroundColor = '#D77FA1';
+    blog.style.color = '#FFEFEF';
+}
+// innertext //
+const specialTag = document.getElementById('special-blog');
+specialTag.innerText = 'pagol hoye jabo'
+// inner html //
+const lauraTag = document.getElementById('laura-blog');
+lauraTag.innerHTML = `
+    <ul>
+        <li>pagol</li>
+        <li>sagol</li>
+        <li>magol</li>
+    </ul>
+    <h1> birini khabo </h1>
+    <p> tomake gaan sunabo </p>
+    <strong>i love u</strong>
+`
+const friends = document.getElemnetById('fri');
